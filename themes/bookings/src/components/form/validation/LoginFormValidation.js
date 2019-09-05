70ed89b809f36ebe4297a70d0d2eec386ff28650
Kartik Patel <kartikpatel@outlook.co.nz>
2019-09-05 16:03:18 +1200

    font awesome and login page validation

---
+++ themes/bookings/src/components/form/validation/LoginFormValidation.js
@@ -0,0 +1,6 @@
+import * as Yup from 'yup';
+
+export const LoginFormValidation = Yup.object().shape({
+    username: Yup.string().email('Invalid email type').required('Please enter a valid email address!'),
+    password: Yup.string().required('Password is required!')
+});
--- themes/bookings/src/init/FrameworkRequirements.js
+++ themes/bookings/src/init/FrameworkRequirements.js
@@ -1,2 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../../node_modules/typeface-open-sans";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCircleNotch)
