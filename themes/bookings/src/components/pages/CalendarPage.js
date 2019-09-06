829eabc3a7b124a2b91109e02c16fcf0cf386b88
Kartik Patel <kartikpatel@outlook.co.nz>
2019-09-06 13:48:07 +1200

    calendar added

--- themes/bookings/src/components/pages/CalendarPage.js
+++ themes/bookings/src/components/pages/CalendarPage.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {Calendar, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
 
-function CalendarPage (){
-        return (
-            <div className="custom-container">
-                fff
-            </div>
-        )
+const localizer = momentLocalizer(moment)
+
+function CalendarPage() {
+    return (
+        <div className="custom-container">
+            <Calendar
+                localizer={localizer}
+                events={[
+                    {
+                      'title': 'My event',
+                      'allDay': false,
+                      'start': new Date(2019, 0, 1, 10, 0), // 10.00 AM
+                      'end': new Date(2019, 0, 1, 14, 0), // 2.00 PM 
+                    }
+                  ]}
+                startAccessor="start"
+                endAccessor="end"
+            />
+        </div>
+    )
 }
 
 export default CalendarPage;
