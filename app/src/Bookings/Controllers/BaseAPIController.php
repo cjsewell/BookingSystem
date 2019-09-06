7b3147156c9fea5f8331e3d64cd43484df307a31
Kartik Patel <kartikpatel@outlook.co.nz>
2019-09-07 10:51:16 +1200

    get logged in user

--- app/src/Bookings/Controllers/BaseAPIController.php
+++ app/src/Bookings/Controllers/BaseAPIController.php
@@ -4,6 +4,8 @@ namespace Bookings\Controllers {
 
     use SilverStripe\Control\Controller;
     use SilverStripe\Control\HTTPResponse;
+    use SilverStripe\Security\PermissionFailureException;
+    use SilverStripe\Security\Security;
 
     /**
      * Class BaseAPIController
@@ -21,6 +23,18 @@ namespace Bookings\Controllers {
 	        return $this->getResponse()
                 ->addHeader('Content-Type', 'application/json')
                 ->setBody(json_encode($res));
+        }
+
+        /**
+         * @return \SilverStripe\Security\Member|null
+         * @throws PermissionFailureException
+         */
+        public function getLoggedInUser(){
+	        $member = Security::getCurrentUser();
+	        if($member){
+	            return $member;
+            }
+	        throw new PermissionFailureException('User is not logged in. Please sign in to access the data');
         }
 	}
 }
